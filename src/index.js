332ffe31a295910e89af3a5c04932c185e77ce84
itstheanurag <gauravanurag36@gmail.com>
2022-04-11 23:11:39 +0530

    Done

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
-const { default: mongoose } = require('mongoose');
+const mongoose = require('mongoose');
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh87698-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://Akshay12:Akshay123@cluster0.eqljz.mongodb.net/gaurav1011-DB", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
 .catch ( err => console.log(err) )
 
-app.use('/', route);
+app.use('/', route);+
 
 
 app.listen(process.env.PORT || 3000, function () {
